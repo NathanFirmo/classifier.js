b58182e22284a375bac4795e53af723fd831335a
NathanFirmo <nathanssfirmo@gmail.com>
2022-09-16 21:25:35 -0300

    test: add afterAll cleanup function

--- tests/classifier.spec.ts
+++ tests/classifier.spec.ts
@@ -1,6 +1,12 @@
 import { Classifier } from '../src/classifier'
+import fs from 'fs'
 
 describe('Classifier', () => {
+  afterAll(async () => {
+    await fs.promises.rm('test.json')
+    await fs.promises.rm('nested-dir', { recursive: true })
+  })
+
   describe('file system methods', () => {
     it('should be able to save data as json in the current directory', async () => {
       const classifier = new Classifier()
